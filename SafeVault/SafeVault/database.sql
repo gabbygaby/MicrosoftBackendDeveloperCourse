5522f56f0cfa10c70f47f22efe50506680384043
Gabriela Gutierrez <gabrigutierrez@outlook.com>
2025-05-21 19:33:21 -0700

    Create database.sql

---
+++ SafeVault/SafeVault/database.sql
@@ -0,0 +1,8 @@
+-- database.sql
+CREATE TABLE IF NOT EXISTS Users (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Role VARCHAR(50) NOT NULL DEFAULT 'user'
+);
